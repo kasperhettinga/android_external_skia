7327158c65fa5a97cea8d40ee880692ad9ce1c0d
hjunii <hjunii@gmail.com>
2014-01-02 14:19:14 +0900

    external skia patch
    Change-Id: I55d8bdb748d7e61e0c3a3eeb72c1c68b0d861b60

--- src/core/SkPaint.cpp
+++ src/core/SkPaint.cpp
@@ -207,9 +207,11 @@ SkPaint::SkPaint() {
 }
 
 SkPaint::SkPaint(const SkPaint& src) {
+#ifdef NEON_BLIT_ANTI_H
     if (sizeof(src) == SIZE_OF_PAINT)
         memcpy_paint_opt((int*)this, (int*)&src);
     else
+#endif
         memcpy((int*)this, (int*)&src, sizeof(src));
 
     SkSafeRef(fTypeface);
